4cb32a27a048d79cf455bde0827898d225076c01
Manuel Kiessling <manuel@kiessling.net>
2012-03-13 17:02:21 +0100

    more concise update loop

--- scripts/app/update.js
+++ scripts/app/update.js
@@ -6,25 +6,18 @@ function() {
   var update = function(world, Enemy, Bullet, Explosion) {
     world.terrainBuilder.update();
 
-    world.players.forEach(function(player) {
-      player.update();
-    });
-
-    world.enemies.forEach(function(enemy) {
-      enemy.update();
-    });
-
-    world.bullets.forEach(function(bullet) {
-      bullet.update();
-    });
-
-    world.explosions.forEach(function(explosion) {
-      explosion.update();
-    });
-
-    world.tiles.forEach(function(tile) {
-      tile.update();
-    });
+    [world.tiles,
+     world.enemies,
+     world.bullets,
+     world.explosions,
+     world.players
+    ].forEach(
+      function(gameElementArray) {
+        gameElementArray.forEach(function(gameElement) {
+          gameElement.update();
+        });
+      }
+    );
 
     world.enemies = world.enemies.filter(function(enemy) {
       return enemy.active;
