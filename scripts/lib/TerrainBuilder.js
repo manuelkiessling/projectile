ab480897d75ba49ae12a64e0280b991cc1f41195
Manuel Kiessling <manuel@kiessling.net>
2012-03-19 23:33:09 +0100

    fixed more require paths

--- scripts/lib/TerrainBuilder.js
+++ scripts/lib/TerrainBuilder.js
@@ -1,5 +1,5 @@
 "use strict";
-define(['draw', 'util'],
+define(['../lib/draw', '../lib/util'],
 
 function(draw, util) {
 
--- scripts/lib/init.js
+++ scripts/lib/init.js
@@ -1,5 +1,5 @@
 "use strict";
-define(['SpriteLoader'],
+define(['../lib/SpriteLoader'],
 
 function(SpriteLoader) {
 
