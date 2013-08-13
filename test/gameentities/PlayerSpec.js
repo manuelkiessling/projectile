19f1c97f0e6388e132066f70249cbd9880dbe1b8
Manuel Kiessling <manuel.kiessling@meinauto.de>
2013-08-13 22:34:13 +0200

    very first rudimentary tests running

--- test/gameentities/PlayerSpec.js
+++ test/gameentities/PlayerSpec.js
@@ -4,7 +4,18 @@ define(['/base/htdocs/js/gameentities/Player.js'], function(Player) {
   describe("Player", function() {
   
     it("can be created", function() {
-      var player = new Player();
+      var mockWorld = {
+        width: 100,
+        height: 100,
+      };
+      var mockOptions = {
+        keyleft: undefined,
+        keyright: undefined,
+        keyup: undefined,
+        keydown: undefined,
+        keyfire: undefined,
+      };
+      var player = new Player(mockWorld, undefined, undefined, mockOptions);
     });
 
   });
