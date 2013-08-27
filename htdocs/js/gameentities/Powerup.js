877f36b5276030436b85e0a127842a7180d4a124
Manuel Kiessling <manuel.kiessling@meinauto.de>
2013-08-27 08:38:33 +0200

    powerup update works in a first simple version

--- htdocs/js/gameentities/Powerup.js
+++ htdocs/js/gameentities/Powerup.js
@@ -3,12 +3,19 @@ define([],
 
 function() {
 
-  var Powerup = function() {
+  var Powerup = function(options) {
     this.active = true;
+    this.x = options.x;
+    this.y = options.y;
+    this.speed = options.speed;
   };
 
   Powerup.prototype.handleCollidedWithPlayer = function() {
     this.active = false;
+  };
+
+  Powerup.prototype.update = function() {
+    this.y = this.y + 1;
   };
 
   return Powerup;
--- test/gameentities/PowerupSpec.js
+++ test/gameentities/PowerupSpec.js
@@ -4,7 +4,11 @@ define(['/base/htdocs/js/gameentities/Powerup.js'], function(Powerup) {
   describe('Powerup', function() {
  
     function createPowerup() {
-      return new Powerup();
+      return new Powerup({
+        x: 0,
+        y: 0,
+        speed: 1,
+      });
     }
 
     it('should be set inactive when colliding with the player', function() {
@@ -12,6 +16,17 @@ define(['/base/htdocs/js/gameentities/Powerup.js'], function(Powerup) {
       expect(powerup.active).toBe(true);
       powerup.handleCollidedWithPlayer();
       expect(powerup.active).toBe(false);
+    });
+
+    it('should move down one speed-unit on each update cycle', function() {
+      var powerup = createPowerup();
+      expect(powerup.y).toBe(0);
+      powerup.update();
+      expect(powerup.y).toBe(1);
+      powerup.update();
+      expect(powerup.y).toBe(2);
+      powerup.update();
+      expect(powerup.y).toBe(3);
     });
 
   });
